e8e2c10567fd6571b1ef5d01115ea3d75af87d7b
eproz <eprozones@gmail.com>
2021-04-10 00:23:46 +0700

    add first Get route

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jull'}
+]
+app.get('/users',function(req, res) {
+    res.json({success: true, message: 'User Mock', users: mockUserData })
+})
 app.listen(8000, function(){
-    console.log('server runnninggit')
+    console.log('server runnning')
 })
