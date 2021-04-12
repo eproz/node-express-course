fa37c3f09dd225b240793744ae989685ec4e1d33
eproz <eprozones@gmail.com>
2021-04-12 22:15:58 +0700

    add login post rule

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const app = express();
+// const bodyparse = require('body-parser')
 const mockUserData = [
     {name: 'Mark'},
     {name: 'Jull'}
 ]
+// app.use(bodyparse.json)
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
+app.post('/login', function(res, req) {
+    const username = res.body.username
+    const password = res.body.password
+
+    const mockUserame = 'biilyThekid'
+    const mockPassword = 'supperSecrect'
+    if (username===mockUserame && password===mockPassword) {
+        req.json({add:ddd})
+    } else {
+        req.json({:=sss: })
+    }
+})
+
 app.get('/users/:id',function(req, res) {
     console.log(req.params.id)
     res.json({success: true, message: 'User Mock', users: mockUserData })
 })
-app.listen(8000, function(){
+
+app.listen(8000 , 'localhost', function(){
     console.log('server runnning')
 })
